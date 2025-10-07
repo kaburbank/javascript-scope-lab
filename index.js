a739bd01d6f73d0e22571055bbe3ad9b2be991d3
Katherine <kaburbank@gmail.com>
2025-10-07 11:02:42 -0600

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Declare variables in the global scope
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+//Write the addBurger function
+function addBurger() {
+  let newBurger = 'Flatburger'; 
+  burgers.push(newBurger); 
+}
+
+//Write if(true) statement for anotherNewBurger
+if (true) {
+  let anotherNewBurger = 'Maple Bacon Burger'; 
+  burgers.push(anotherNewBurger);
+}
+
+//Write the changeFeatureDrink function
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake'; 
+}
+
+ 
